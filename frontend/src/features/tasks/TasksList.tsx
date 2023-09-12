a04437cf3485592bbf2e6bf1788fdc7ada644f2f
Anton Sipin <anton.sipin@gmail.com>
2023-09-12 03:50:56 +0300

    Refactoring

--- frontend/src/features/tasks/TasksList.tsx
+++ frontend/src/features/tasks/TasksList.tsx
@@ -13,11 +13,13 @@ import { TasksTable } from '../../components/TasksTable'
 import { MaterialTable } from '../../components/TasksTable'
 import { Spinner } from '../../components/Loader'
 import cn from 'classnames'
+import Button from '../../components/Button'
 const URL = 'ws://localhost:3100'
 
 export default function TasksList(): JSX.Element {
   const { info, error, tasks,  handleCreateTask, handleLoadTasks, handleInfo, handleError, handleUpdate, handleDelete, handleHide, handleResolve } = useTasks()
   const [theme, setTheme] = useState('White')
+  const [tableMode, setTableMode] = useState(false)
 
   useEffect(() => {
     const ws = new WebSocket(URL)
@@ -42,23 +44,27 @@ export default function TasksList(): JSX.Element {
             <option value='White'>White</option>
             <option value='Black'>Black</option>
         </select>
+        <Button 
+        onClick={() => setTableMode(!tableMode)} 
+        children={'Switch mode'} 
+        btnType={'mode'}
+      />
       </div>
       <Info onHandleInfo={handleInfo} info={info}  />
       <Form onHandleSubmit={handleSubmit} onHandleError={handleError} error={error}/>
-
-      {tasks.length ? 
-      <div
-      className={cn(styles.tableWrapper)}
-      >
-        <MaterialTable tasks={tasks} />
-      </div> :
-      <div className={styles.loader}>
-      <Spinner />
-      </div> || <NoTasks />
-    }
       
-
-      {/* <div className={styles.taskWrapper}>
+      {tableMode ? (
+        tasks.length  ?
+          <div
+          className={cn(styles.tableWrapper)}
+          >
+            <MaterialTable tasks={tasks} />
+          </div> :
+          <div className={styles.loader}>
+          <Spinner />
+          </div> || <NoTasks />
+      ) : (
+        <div className={styles.taskWrapper}>
         {
             tasks.length ?  tasks.map(
               (task: Task) => <TaskComponent key={task.id} task={task} onHandleUpdate={handleUpdate} onHandleDelete={handleDelete} onHandleHide={handleHide} onHandleResolve={handleResolve} className={theme}/>
@@ -67,7 +73,13 @@ export default function TasksList(): JSX.Element {
                 <Spinner />
               </div> || <NoTasks />
         }
-      </div> */}
+      </div>
+      )
+      }
+      
+      
+
+      
     </div>
   )
 }
