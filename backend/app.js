eb00ca4e350333a09f01d675759db063dacc5f2e
Anton Sipin <anton.sipin@gmail.com>
2023-10-26 20:37:04 +0300

    fix: proxy

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,11 @@ app.use(
 )
 app.use(methodOverride('_method'))
 app.use(userMiddle.userName)
-app.use('/', mainRouter)
-app.use('/task', userMiddle.isAuth, taskRouter)
+app.use('/', indexRouter)
+// app.use('/task', userMiddle.isAuth, taskRouter)
+app.use('/task', taskRouter)
 app.use('/user', userRouter)
-// app.use('*', mainRouter)
+app.use('*', mainRouter)
 
 server.on('request', app)
 server.listen(PORT, () => {
