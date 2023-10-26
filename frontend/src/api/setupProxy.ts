1e9cdb5b5e31663211905b02ee26ff7f51a6d58c
Anton Sipin <anton.sipin@gmail.com>
2023-10-26 15:54:04 +0300

    fixed proxy

--- frontend/src/api/setupProxy.ts
+++ frontend/src/api/setupProxy.ts
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function(app: { use: (arg0: any) => void }) {
     app.use(createProxyMiddleware('/api', {target: 'http://localhost:3100'}))
 }
